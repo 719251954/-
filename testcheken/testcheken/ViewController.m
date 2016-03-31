f14df9b3996f9de75093ce56f33e3c4730d8a132
zs <zs@126.com>
2016-03-31 11:42:04 +0800

    注释

---
+++ testcheken/testcheken/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  testcheken
+//
+//  Created by qianfeng on 16/3/31.
+//  Copyright © 2016年 WuLei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //sdfds
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
